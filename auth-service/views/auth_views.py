df38eb3435107f7f9081168d3b9914d69bd7d114
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2023-11-02 16:39:21 -0600

    refactored the authview codes with url breakdown

--- auth-service/views/auth_views.py
+++ auth-service/views/auth_views.py
@@ -15,7 +15,10 @@ async def login(request: Request):
     body_bytes = await request.body()
     body_str = body_bytes.decode("utf-8")  # Decode the bytes to a string
     data = json.loads(body_str)  # Deserialize the JSON data
-    DJANGO_API_URL = "http://user-service:8000/user-service/api/v1/account/login-validate/"
+    host_url = "http://user-service:8000"
+    user_service_tag = "/user-service/"
+    url_for_login_validate = "api/v1/account/login-validate/"
+    DJANGO_API_URL = host_url + user_service_tag + url_for_login_validate
     response = ""
     try:
         response = requests.post(DJANGO_API_URL, data=data)
