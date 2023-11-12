8309265550974985195c81ee26e8fda55f279af0
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2023-11-12 03:12:34 -0600

    react client can now login, nginx configuration added with cors header

---
+++ react-client/src/components/List.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+import { useAppSelector } from '../store/store';
+
+const List: FC = () => {
+  const persons = useAppSelector((state) => state.person.persons);
+
+  return (
+    <div>
+      <p>This is a list component</p>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {persons.map((person) => (
+            <tr key={person.id}>
+              <td>{person.id}</td>
+              <td>{person.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default List;
