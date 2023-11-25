c82900290b44e50900b3650e4c027cb7c7a6e141
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2023-11-25 17:01:41 -0600

    added sphinx documentation with nginx

--- swagger-ui/src/app.js
+++ swagger-ui/src/app.js
@@ -8,14 +8,10 @@ const swaggerDocument = YAML.load('imdb-movie-openapi3.0-schema.yaml');
 
 const port = 3002;
 // Middleware for /docs
-app.use('/docs', router);
+app.use('/api-docs', router);
 
-// Define routes under /docs
-router.get('/', (req, res) => {
-  res.send('Hello, World22!');
-});
 
-router.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
