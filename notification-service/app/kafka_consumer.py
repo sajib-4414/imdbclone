8be1885ab392e89b28f43315d21cf2b19e255202
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2023-12-13 18:22:22 +0000

    fastapi now has asyncronous aiokafka listener

--- notification-service/app/kafka_consumer.py
+++ notification-service/app/kafka_consumer.py
@@ -1,23 +1,42 @@
 # kafka_consumer.py
-from kafka import KafkaConsumer
+from aiokafka import AIOKafkaConsumer
 import pickle
 from app.topics import TOPIC_USER_REGISTERED
-def kafka_consumer():
-    print("listening to kafka events on the notification service")
+from events.listeners import handle_user_registered_event
+async def kafka_consumer():
+    print("Listening to Kafka... On Notification service...")
     topics_all = [TOPIC_USER_REGISTERED]
-    consumer = KafkaConsumer(*topics_all, 
-        bootstrap_servers=['kafka:9092'], 
-        api_version=(0, 10) 
-        #,consumer_timeout_ms=1000
-        )
 
-    for message in consumer:
-        process_kafka_event(message)
+    consumer = AIOKafkaConsumer(
+        *topics_all,
+        bootstrap_servers='kafka:9092',
+        group_id="notification_group")
+    # Get cluster layout and join group `my-group`
+    await consumer.start()
 
-def process_kafka_event(message):
+    try:
+        # Consume messages
+        async for message in consumer:
+            await process_kafka_event(message)
+            # print("consumed: ", msg.topic, msg.partition, msg.offset,
+            #       msg.key, msg.value, msg.timestamp)
+    except Exception as e:
+    # Handle the error and print a generic message
+        print(f"An error occurred during reading kafka message on the notificaiton consumer: {e}")
+
+    finally:
+        pass
+        # Will leave consumer group; perform autocommit if enabled.
+        # await consumer.stop()
+
+    # async for message in consumer:
+    #     asyncio.create_task(process_kafka_event(message))
+        # process_kafka_event(message)
+
+async def process_kafka_event(message):
     topic = message.topic
     deserialized_data = pickle.loads(message.value)
     print("message received on the notification service") 
     print(deserialized_data)
-    # if topic == TOPIC_USER_REGISTERED:
-    #     handle_user_registered_event(deserialized_data)
+    if topic == TOPIC_USER_REGISTERED:
+        await handle_user_registered_event(deserialized_data)
--- notification-service/events/listeners.py
+++ notification-service/events/listeners.py
@@ -1,13 +1,14 @@
 from events.user_created import UserCreatedEvent
-from app.db import database, User
+from app.db import User
+import sqlalchemy 
 
 async def handle_user_registered_event(data):
     event = UserCreatedEvent(**data)
-    print("successfully received user created event in Movie service..., will create user now.")
+    print("will create a same user now here..")
     print(f"username= {event.username}")
     print(f"email={event.username}")
     try:
         user = await User.objects.create(username=event.username, email=event.email)
         print(f"user created on the Notification service with email {event.email}")
-    except django.db.IntegrityError as e:
-        print(f"Error creating user on the movie service, user with credentials exists already")
+    except sqlalchemy.exc.IntegrityError as e:
+        print("Error creating user on the notification service. User with credentials exists already.")
