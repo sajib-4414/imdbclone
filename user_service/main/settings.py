228a1288a63fbe55cc8e873d60d31bfdeb59a1a3
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2023-12-16 01:22:22 -0600

    user service has now postgres db

--- user_service/main/settings.py
+++ user_service/main/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'user_db',
+        'USER': 'root',
+        'PASSWORD':'root',
+        'HOST': 'user-db',
+        'PORT': '5432'
     }
 }
 
